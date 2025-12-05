74d6f47de0477cf42ab0803c1afbc31219b2a468
lanye74 <layne.ogden727@gmail.com>
2025-12-05 03:11:21 -0500

    some basic styling

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,29 @@
 <script lang="ts">
+	import "@fontsource/pt-serif";
+	import "@fontsource-variable/funnel-sans";
+
+
+
 	let {children} = $props();
 </script>
 
-{@render children()}
+<style>
+	:global(body) {
+		margin: 0;
+
+		padding-block: 2em;
+		padding-inline: calc(calc(100vw - 42em) / 2);
+
+		line-height: 1.6;
+	}
+
+	main {
+		padding: 2em;
+		border: 0.125em solid #eee;
+		box-shadow: 0 0;
+	}
+</style>
+
+<main>
+	{@render children()}
+</main>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,51 @@
-<p>
-	yippee
-</p>
+<style>
+	section {
+		& > h2 {
+			font-family: "Funnel Sans Variable";
+			font-weight: 700;
+		}
+
+		& blockquote {
+			font-family: "PT Serif";
+		}
+	}
+
+	h2 {
+		margin-block: 1em 1em;
+	}
+
+	blockquote {
+		margin: 0;
+		margin-block-start: 1em;
+		margin-inline-start: 1em;
+
+		font-style: italic;
+
+		&::before {
+			content: "\“";
+			position: absolute;
+
+			transform: translate(-0.5em, -0.5em);
+
+			font-style: normal;
+			color: #777;
+			font-weight: bold;
+			font-size: 2em;
+		}
+	}
+
+	p {
+		margin-block-end: 1em;
+	}
+</style>
+
+
+<main>
+	<section>
+		<h2>Why?</h2>
+		<blockquote>
+			<p>The reason I choose to become an engineer hasn't changed: I have an insatiable love of learning, and computer science is such a vast field. There is so much to do, and programming in particular satisfies a number of my core needs: with the problem-solving it requires, the pattern recognition is necessitates, the freedom to create it grants, and the self-expression it thus grants. There's no right way to do computer science. I focus on web design and accessibility, but just as easily, one could focus on embedded systems, databasing, cybersecurity, data science, programming language design, machine learning, operating systems, graphics…</p>
+			<p>The options are innumerable, and new fields continue to emerge. I will never go hungry, and being an engineer means being surrounded by like-minded people with the same love of learning. Plus, I get to pick up bits and pieces from every new person I interact with, as well :-)</p>
+		</blockquote>
+	</section>
+</main>
