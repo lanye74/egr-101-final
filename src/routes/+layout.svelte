2a9a1661188934bdf495f20812cdf7490b192355
lanye74 <layne.ogden727@gmail.com>
2025-12-05 03:25:28 -0500

    dump the other qs

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -8,19 +8,37 @@
 </script>
 
 <style>
+	:root {
+		--spacing-l: 2em;
+		--spacing-m: 1em;
+
+		--font-size-l: 2em;
+		--font-size-m: 1em;
+
+		--color-light: #eee;
+		--color-gray: #aaa;
+	}
+
 	:global(body) {
 		margin: 0;
 
-		padding-block: 2em;
-		padding-inline: calc(calc(100vw - 42em) / 2);
+		padding-block: var(--spacing-l);
+		padding-inline: max(var(--spacing-l), calc(calc(100vw - 42em) / 2));
 
 		line-height: 1.6;
+
+		font-family: "PT Serif";
 	}
 
+	:global(h1, h2, h3) {
+		font-family: "Funnel Sans Variable";
+	}
+
+
+
 	main {
-		padding: 2em;
-		border: 0.125em solid #eee;
-		box-shadow: 0 0;
+		padding: var(--spacing-l);
+		border: 0.125em solid var(--color-light);
 	}
 </style>
 
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,51 +1,72 @@
 <style>
-	section {
-		& > h2 {
-			font-family: "Funnel Sans Variable";
-			font-weight: 700;
-		}
-
-		& blockquote {
-			font-family: "PT Serif";
-		}
+	h2 {
+		margin-block: var(--spacing-m) var(--spacing-m);
+		font-size: var(--font-size-l);
 	}
 
-	h2 {
-		margin-block: 1em 1em;
+	p {
+		margin-block-end: var(--spacing-m);
 	}
 
 	blockquote {
 		margin: 0;
-		margin-block-start: 1em;
-		margin-inline-start: 1em;
+		margin-block-start: var(--spacing-m);
+		margin-inline-start: var(--spacing-m);
 
 		font-style: italic;
 
 		&::before {
+			--spacing: calc(-1 / 2 * var(--spacing-m));
+
 			content: "\“";
 			position: absolute;
 
-			transform: translate(-0.5em, -0.5em);
+			transform: translate(var(--spacing), var(--spacing));
 
+			color: var(--color-gray);
 			font-style: normal;
-			color: #777;
 			font-weight: bold;
-			font-size: 2em;
-		}
-	}
 
-	p {
-		margin-block-end: 1em;
+			font-size: var(--font-size-l);
+		}
 	}
 </style>
 
 
 <main>
 	<section>
+		<h2>The roadmap will go here</h2>
+		<p>Promise!</p>
+	</section>
+	<section>
 		<h2>Why?</h2>
 		<blockquote>
 			<p>The reason I choose to become an engineer hasn't changed: I have an insatiable love of learning, and computer science is such a vast field. There is so much to do, and programming in particular satisfies a number of my core needs: with the problem-solving it requires, the pattern recognition is necessitates, the freedom to create it grants, and the self-expression it thus grants. There's no right way to do computer science. I focus on web design and accessibility, but just as easily, one could focus on embedded systems, databasing, cybersecurity, data science, programming language design, machine learning, operating systems, graphics…</p>
 			<p>The options are innumerable, and new fields continue to emerge. I will never go hungry, and being an engineer means being surrounded by like-minded people with the same love of learning. Plus, I get to pick up bits and pieces from every new person I interact with, as well :-)</p>
+		</blockquote>
+	</section>
+
+	<section>
+		<h2>A CS Major</h2>
+		<blockquote>
+			<p>My desired major remains computer science. I simply love programming, and it just makes me whole. Not that programming is everything there is to computer science!—but the ability to problem-solve, to apply my pattern recognition, to think like a computer, to create tools for my own ease of use, to build products and express myself, and the vast amount there is to learn simply sates all of my core needs.</p>
+
+			<p>And for all that I already know, there is still so much more. To further my web design skills, there are a few goals I have: to learn a proper design tool, to learn a stylesheet language (like SCSS or Tailwind), to learn more deeply into building and architecting databases, to dig deeper into accessibility, and simply to build more and gain more experience. In doing so, I can push further to my end-goal of user-first design.</p>
+
+			<p>I think the biggest attribute of successful UX designers is empathy: to be able to listen and understand the needs of the user. Which seems at odds with a lot of the stereotypes we hold about programmers, making it doubly important. A lot of developers can build interfaces that work, but making them both pretty and accessible is really difficult. Designing for screenreaders is an entire added layer of complexity that requires dedication and forethought.</p>
+
+			<p>I think the path to learning web design is clear, but as far as computer science as whole goes, there are so many ways to succeed.</p>
+		</blockquote>
+	</section>
+
+	<section>
+		<h2>In The Real World</h2>
+		<blockquote>
+			<p>As part of my focus on user-centric design, to me, being a successful engineer means making any project that I touch better for its users. There are too many websites that work but are horrible for the end-user. I like to pick on myUK, which… may not be fair given its age. But it is a prime target: the content layout with tab systems is awkward, it contains a number of embedded pages that you can't use the forward or back navigation buttons on, and it isn't keyboard accessible, let alone screenreader-accessible, just to name a few.</p>
+
+			<p>I want to make products that are user-focused inside and out; that are both beautiful and accessible to people of all levels of access. Products that don't make people sigh that I guess that's the way it is. A professional position I seek is one that allows me to improve the hassle of daily life over and over. Ideally, my work would be completely unnoticed all together. I also hope to be constantly challenged; to never get bored of what I do, or get complacent, and even better to push me out of my comfort zone. A job that both terrifies me and excites me.</p>
+
+			<p>In the following 10 years, I hope that I get to keep doing what I love, without ever feeling stagnant. The thing I fear the most is getting relegated to a managerial position where I no longer get to write code or solve problems.</p>
 		</blockquote>
 	</section>
 </main>
